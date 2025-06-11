868dcc3c71b83aac9a3f7db6b60f0cd13afa2891
Soumadeep Dey <dey180343@gmail.com>
2025-06-12 02:37:05 +0530

    New one

---
+++ Basic Maths/Print_Divisors.java
@@ -0,0 +1,44 @@
+// You are given an integer n. You need to find all the divisors of n. Return all the divisors of n as an array or list in a sorted order.
+
+
+
+// A number which completely divides another number is called it's divisor.
+
+
+// Examples:
+// Input: n = 6
+
+// Output = [1, 2, 3, 6]
+
+// Explanation: The divisors of 6 are 1, 2, 3, 6.
+
+// Input: n = 8
+
+// Output: [1, 2, 4, 8]
+
+// Explanation: The divisors of 8 are 1, 2, 4, 8.
+
+
+import java.util.*;
+
+public class Print_Divisors {
+    public int[] divisors(int n)
+    {
+        List<Integer> ls= new ArrayList<>(); // similar to vector
+        for(int i=1;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                ls.add(i); // similar to push_back();
+                if((n/i)!=i)
+                    ls.add(n/i);
+            }
+        }
+        Collections.sort(ls); // similar to sort();
+
+        int[] res=new int[ls.size()];
+        for(int i=0;i<ls.size();i++)
+            res[i]=ls.get(i); // similar to *it or v[0] if v is a vector
+        return res;
+    }
+}
