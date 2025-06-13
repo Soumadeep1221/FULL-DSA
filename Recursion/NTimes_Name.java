500dedb75573cb6e7591d8e13a5373ef4e483b20
Soumadeep Dey <dey180343@gmail.com>
2025-06-13 15:04:20 +0530

    Recursion Part 1

---
+++ Recursion/NTimes_Name.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class NTimes_Name
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a name:-");
+        String str=sc.nextLine();
+        System.out.println("Enter the no. of iterations:-");
+        int n=sc.nextInt();
+        sc.close();
+        print(str,n);
+    }
+    public static void print(String str, int n)
+    {
+        if(n==1)
+        {
+            System.out.println(str);
+        }     
+        else
+        {
+            System.out.println(str);
+            print(str,--n);
+        }
+    }
+}
+    
