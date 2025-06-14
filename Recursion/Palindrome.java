51078e34b459bfe50995685fe449aa9b6ff817b7
Soumadeep Dey <dey180343@gmail.com>
2025-06-15 00:10:29 +0530

    Some edits

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -7,7 +7,7 @@ public class Palindrome {
         System.out.println("Enter a string:-");
         String str=sc.nextLine();
         sc.close();
-        if(palindromeCheck(str,0))
+        if(palindromeCheck(str))
             System.out.println(str+" is a palindrome.");
         else
             System.out.println(str+" is not a palindrome.");
@@ -16,15 +16,24 @@ public class Palindrome {
     // 1) If I use one extra array to store the reversed array inside the recursive function --> Here the T.C = O(N) but the S.C wil be more in this case as I am using an extra array to reverse.
 
 
-    public static boolean palindromeCheck(String s,int i) // 2) Here the T.C = O(N/2)= O(N) (Because in Big-O-Notation we generally ignore the constant factors) but the S.C wil be less in this case as I am not using an extra array to reverse. i.e S.C -> O(N)
+    // public static boolean palindromeCheck(String s,int i) // 2) Here the T.C = O(N/2)= O(N) (Because in Big-O-Notation we generally ignore the constant factors) but the S.C wil be less in this case as I am not using an extra array to reverse. i.e S.C -> O(N)
+    // {
+    //     int size=s.length();
+    //     s=s.toLowerCase();
+    //     if(i>=size/2) // You can give i==size/2 it doesn't matter
+    //         return true;
+    //     if(s.charAt(i)!=s.charAt(size-1-i))
+    //         return false;
+    //     return palindromeCheck(s, ++i);
+    // }
+    public static boolean palindromeCheck(String s) // 3) Here only the string is passed. T.C = O(N/2)= O(N) =S.C
     {
-        int size=s.length();
         s=s.toLowerCase();
-        if(i>=size/2)
+        if(s.length()<=1)
             return true;
-        if(s.charAt(i)!=s.charAt(size-1-i))
+        if(s.charAt(0)!=s.charAt(s.length()-1))
             return false;
-        return palindromeCheck(s, ++i);
+        return palindromeCheck(s.substring(1,s.length()-1));
     }
 }
 
--- Recursion/R_Array.java
+++ Recursion/R_Array.java
@@ -25,7 +25,7 @@ public class R_Array {
 
     public static void revarr(int[] arr,int n,int i)  // 2) Here the T.C = O(N/2)= O(N) (Because in Big-O-Notation we generally ignore the constant factors) but the S.C wil be less in this case as I am not using an extra array to reverse. i.e S.C -> O(N)
     {
-        if(i==n/2)
+        if(i==n/2) // You can give i>=n/2 it doesn't matter
             return;
         else
         {
@@ -38,12 +38,12 @@ public class R_Array {
 }
 
 
-// public void revarr(int[] arr, int n) { // 3) Here only the array and the it's size are passed. T.C = O(N/2)= O(N) =S.C
+// public static void revarr(int[] arr, int n) { // 3) Here only the array and the it's size are passed. T.C = O(N/2)= O(N) =S.C
 //         int size = arr.length;
 //         int i = size - n;
 //         int j = n - 1;
 
-//         if (i >= j) return; // Prevent over-swapping
+//         if (i >= j) return; // You can give i>=n/2 it doesn't matter
 
 //         int temp = arr[i];
 //         arr[i] = arr[j];
